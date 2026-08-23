f6bf905058a4ae04d135b5166e4dd772ac59c9d8
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-23 17:38:01 +0330

    Create payment.js

---
+++ src/config/payment.js
@@ -0,0 +1,13 @@
+/**
+ * Payment configuration
+ *
+ * مسیر:
+ * src/config/payment.js
+ *
+ * اطلاعات کارت ثابت است و ارتباطی با Blupal ندارد.
+ */
+
+export const PAYMENT_CONFIG = Object.freeze({
+  CARD_NUMBER: '6219861808375991',
+  CARD_HOLDER: 'مهدی عاقبتی',
+});
