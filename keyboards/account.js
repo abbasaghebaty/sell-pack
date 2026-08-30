54f43a30eccbd1ad33dc52b664d62ec9c248cc6e
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-30 11:40:41 +0330

    Update account.js

--- keyboards/account.js
+++ keyboards/account.js
@@ -1,26 +1,41 @@
-export const ACCOUNT_BUTTONS =
-  Object.freeze({
-    BACK: '🔙 بازگشت',
-    TOPUP: '💳 شارژ کیف پول',
-  });
+/**
+ * Account Keyboard
+ *
+ * مسیر:
+ * keyboards/account.js
+ */
+
+export const ACCOUNT_BUTTONS = Object.freeze({
+  TOPUP: '💳 شارژ کیف پول',
+  BACK: '🔙 بازگشت',
+});
 
 export function getAccountKeyboard() {
   return {
     inline_keyboard: [
       [
         {
-          text:
-            ACCOUNT_BUTTONS.TOPUP,
-          callback_data:
-            'wallet_topup_start',
+          text: ACCOUNT_BUTTONS.TOPUP,
+          callback_data: 'wallet_topup_start',
         },
       ],
       [
         {
-          text:
-            ACCOUNT_BUTTONS.BACK,
-          callback_data:
-            'account_back',
+          text: ACCOUNT_BUTTONS.BACK,
+          callback_data: 'account_back',
+        },
+      ],
+    ],
+  };
+}
+
+export function getWalletTopupInputKeyboard() {
+  return {
+    inline_keyboard: [
+      [
+        {
+          text: '❌ لغو',
+          callback_data: 'wallet_topup_cancel_current',
         },
       ],
     ],
