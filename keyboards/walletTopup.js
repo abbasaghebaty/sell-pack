ea2986894cc58dc87efa78cd7538f2a0892b7099
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-08-30 11:40:57 +0330

    Update walletTopup.js

--- keyboards/walletTopup.js
+++ keyboards/walletTopup.js
@@ -1,11 +1,17 @@
+/**
+ * Wallet Top-up Keyboard
+ *
+ * مسیر:
+ * keyboards/walletTopup.js
+ */
+
 export function createCancelTopupKeyboard() {
   return {
     inline_keyboard: [
       [
         {
           text: '❌ لغو شارژ',
-          callback_data:
-            'wallet_topup_cancel_current',
+          callback_data: 'wallet_topup_cancel_current',
         },
       ],
     ],
